5192cc952458432598b8a3630df7000e2f2ba1e5
liaolongcheng <ios_coder@126.com>
2014-08-06 14:31:32 +0800

    Initial Commit

--- swift/typealias/typealias/main.swift
+++ swift/typealias/typealias/main.swift
@@ -2,11 +2,13 @@
 //  main.swift
 //  typealias
 //
-//  Created by liaolongcheng on 14-8-6.
-//  Copyright (c) 2014年 liaolongcheng. All rights reserved.
+//  Created by liaolongcheng on 13-7-30.
+//  Copyright (c) 2013年 liaolongcheng. All rights reserved.
 //
 
 import Foundation
 
-println("Hello, World!")
-
+/**类型别名 在swift中我们可以用typealias来定义自己的数据类型 类似于oc中使用的typedef*/
+typealias ID = Array<String> //这样就定义了我们自己的一个ID类型
+var myClassName:ID = ["张三三","李四","王五"] //使用自己定义的ID类型来声明一个变量
+println(myClassName)
---
+++ swift/weater/weater/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weater
+//
+//  Created by liaolongcheng on 14-8-6.
+//  Copyright (c) 2014年 liaolongcheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
