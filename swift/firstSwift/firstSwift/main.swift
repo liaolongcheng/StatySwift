f77f2e6354abf7ef8689d779c34b16dc65f1fbc8
liaolongcheng <ios_coder@126.com>
2014-08-06 14:29:06 +0800

    Initial Commit

--- swift/firstSwift/firstSwift/main.swift
+++ swift/firstSwift/firstSwift/main.swift
@@ -2,11 +2,32 @@
 //  main.swift
 //  firstSwift
 //
-//  Created by liaolongcheng on 14-8-6.
-//  Copyright (c) 2014年 liaolongcheng. All rights reserved.
+//  Created by liaolongcheng on 13-7-30.
+//  Copyright (c) 2013年 liaolongcheng. All rights reserved.
 //
 
 import Foundation
 
-println("Hello, World!")
+/*可以在声明的时候指定类型当我们没有指定变量类型那么系统会自动根据值的类型来推导变量类型
+之后我们不能赋值其他类型
+*/
+//声明一个变量
+var myVariable = 42
+/**
+错误实例
+myVariable = "myString" 编译会报错
+*/
+
+//声明一个常量
+let myConstant = 12
+
+/**指定数据类型*/
+var myAge:Int = 22
+
+/**Swift不会给变量赋初始值 在声明变量时必须要进行赋值*/
+var myPhone
+
+
+
+
 
